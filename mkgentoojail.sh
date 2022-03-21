1e21437a1ae2e8b747c494d4d1ab2013fc99bb2f
Alexander Gattin <xrgtn@yandex.ru>
2022-03-21 18:58:13 +0000

    implement update/enter usage
    * move jail creation code into mkjail() function
    * add "update" and "enter" operations
    * add "create" variations for firefox/telegram
    * generate subnet from jail directory name, use 192.168.0.0 if
      unsuccessful
    * move hardcoded username "foobar" and uid 60000 to JUSR and JNET
      parameters
    * automatically unmount /var/db/repos etc on completion

--- mkgentoojail.sh
+++ mkgentoojail.sh
@@ -27,14 +27,15 @@
 #    /var/cache/distfiles (and optionally /var/cache/binpkgs and /usr/src) will
 #    be bind mounted from the outer system.
 
-# Default network is 192.168.0.0/24:
-JNET="192.168.0"
 # Default Gentoo arch/profile:
 ARCH="amd64"
 PROF="hardened-nomultilib-selinux-openrc"
 # Default Gentoo download server:
 # DSRV="https://distfiles.gentoo.org"
 DSRV="https://bouncer.gentoo.org/fetch/root/all"
+# Default inmate user name and uid:
+JUSR="foobar"
+JUID=60000
 
 # Accepts 1 or 2 arguments:
 #   die [errcode] message_pt1 [message_pt2 ...]
@@ -52,7 +53,7 @@ die() {
     exit "$E"
 }
 
-# USAGE: chmod_chown "$FN" "$MODE" "$OWNER"
+# USAGE: chmod_chown FILENAME MODE OWNER
 chmod_chown() {
 	[ -e "$1" ] || die
 	case "z$2" in
@@ -157,32 +158,6 @@ esac
 if ! [ -e "$1" ]; then die "$1 does not exist"; fi
 if ! [ -d "$1" ]; then die "$1 is not a directory"; fi
 
-# Change to jail directory and get its absname:
-cd "$1" || die
-JAIL="`pwd`"
-case "z$JAIL" in
-z) die "'pwd' error";;
-z/) die "jail dir cannot be /";;
-esac
-
-# Strip trailing slash:
-JAIL="${JAIL%/}"
-
-# Strip leading dirname:
-JNAM="${JAIL##*/}"
-
-# Unmount jail subdirectories/submounts:
-while read A B C; do
-	case "z$B" in
-	z"$JAIL"/*)
-		umount --recursive "$B" || die
-		;;
-	esac
-done </proc/self/mounts
-
-# Clean all files/directories in jail:
-find . -mount -maxdepth 1 ! -name . -execdir rm -rf '{}' '+'
-
 # Create safe tmp directory and ensure its autoremoval on exit:
 TDIR="/tmp/tmp-mkgentoojail-$$"
 if ! mkdir -m 0750 "$TDIR" ; then
@@ -195,69 +170,177 @@ rm_rf_tdir() {
 }
 trap rm_rf_tdir EXIT
 
-# Download latest-stage3-xxx.txt file, e.g.
-# latest-stage3-amd64-hardened-nomultilib-selinux-openrc.txt:
-#   # Latest as of Sun, 20 Mar 2022 18:00:03 +0000
-#   # ts=1647799203
-#   20220315T091810Z/stage3-amd64-hardened-nomultilib-selinux-openrc-20220315T091810Z.tar.xz 221397584
-wget -O "$TDIR/STAGE3.txt" \
-	"$DSRV"/releases/"$ARCH"/autobuilds/latest-stage3-"$ARCH"-"$PROF".txt \
-	|| die
-
-# Find filename/URL of latest stage3-xxx.tar.xz:
-TXZ=""
-while read A B; do
-	case "z$A" in
-	z"#"*)	continue;;
-	z*stage3*"$ARCH"-"$PROF"*.tar.xz)
-		TXZ="$A"
-		break;;
+# USAGE: chdir_jail path/to/jail
+#
+# on return:
+#	JAIL	- /abs/path/to/jail
+#	JNAME	- jail_basename
+#	JNET    - 192.168.X (0 <= X <= 255)
+chdir_jail() {
+	# Change to jail directory and get its absname:
+	cd "$1" || die
+	JAIL="`pwd`"
+	case "z$JAIL" in
+	z) die "'pwd' error";;
+	z/) die "jail dir cannot be /";;
 	esac
-done <"$TDIR/STAGE3.txt"
-[ "z$TXZ" != "z" ] || die "cannot find latest stage3 tar.xz"
 
-# Basename of tar.xz:
-TXZN="${TXZ##*/}"
+	# Strip trailing slash:
+	JAIL="${JAIL%/}"
 
-# Download tar.xz and signature files:
-wget -cO "/var/tmp/$TXZN" \
-	"$DSRV"/releases/"$ARCH"/autobuilds/"$TXZ" \
-	|| die
-wget -O "/var/tmp/$TXZN".asc \
-	"$DSRV"/releases/"$ARCH"/autobuilds/"$TXZ".asc \
-	|| die
+	# Strip leading dirname:
+	JNAM="${JAIL##*/}"
 
-# Verify signature:
-gpg --verify "/var/tmp/$TXZN".asc || die
+	# Convert last digits in jail name (if any) to component X of
+	# jail's subnet (192.168.X.0/24):
+	X=`printf '%s' "$JNAM" \
+		| sed -nr 's/^(.*[^0-9])?([0-9]+)[^0-9]*$/\2/p'`
+	case "z$X" in z) X=0;; esac
+	if [ "$X" -gt 255 ]; then
+		die "invalid JNET: 192.168.$X.0/24";
+	fi
+	JNET="192.168.$X"
 
-# Extract tar.xz in jail dir:
-printf 'Extracting %s\n' "$TXZN"
-tar xpJf "/var/tmp/$TXZN" --acls --xattrs --xattrs-include='*.*' \
-	--numeric-owner --selinux --atime-preserve=system \
-	|| die
+	# Unmount jail subdirectories/submounts:
+	while read A B C; do
+		case "z$B" in
+		z"$JAIL"/*)
+			umount --recursive "$B" || die
+			;;
+		esac
+	done </proc/self/mounts
+}
 
-# Remove dev subdirectory and re-create it from scratch:
-rm -rf ./dev || die
-printf 'Re-creating minimal %s\n' "$JAIL/dev"
-mkdev ./
+# USAGE: mount_jail_var JAIL JNAM
+mount_jail_var() {
+	# Mount /var/tmp/portage:
+	mkdir /var/tmp/portage-"$JNAM"
+	chown portage:portage /var/tmp/portage-"$JNAM"
+	chmod 0755 /var/tmp/portage-"$JNAM"
+	mkdir "$JAIL/var/tmp/portage"
+	chown portage:portage "$JAIL/var/tmp/portage"
+	chmod 0755 "$JAIL/var/tmp/portage"
+	mount --bind /var/tmp/portage-"$JNAM" "$JAIL/var/tmp/portage"
+	mount --make-slave "$JAIL/var/tmp/portage"
 
-# Do minimal necessary configuration of portage in jail:
-# This is required by firefox:
-cat >"$JAIL/etc/portage/package.use/local.use" <<EOF
-x11-libs/cairo		X
-media-libs/libvpx	postproc
+	# Mount /var/db/repos:
+	mount --bind /var/db/repos "$JAIL/var/db/repos"
+	mount --make-slave "$JAIL/var/db/repos"
+
+	# Mount /var/cache/distfiles:
+	mount --bind /var/cache/distfiles "$JAIL/var/cache/distfiles"
+	mount --make-slave "$JAIL/var/cache/distfiles"
+}
+
+# USAGE: umount_jail_var JAIL JNAM
+umount_jail_var() {
+	umount --recursive "$JAIL/var/db/repos"
+	umount --recursive "$JAIL/var/cache/distfiles"
+	umount --recursive "$JAIL/var/tmp/portage"
+	rm -rf /var/tmp/portage-"$JNAM"
+}
+
+# USAGE: mkjail path/to/jail ARCH PROF DSRV JUSR JUID firefox
+#    or  mkjail path/to/jail ARCH PROF DSRV JUSR JUID telegram
+#    or  mkjail path/to/jail ARCH PROF DSRV JUSR JUID
+mkjail() {
+	chdir_jail "$1"
+	[ "z$JAIL" != "z" ] || die
+	[ "z$JNAM" != "z" ] || die
+	[ "z$JNET" != "z" ] || die
+	ARCH="$2"
+	PROF="$3"
+	DSRV="$4"
+	JUSR="$5"
+	JUID="$6"
+	expr match "$JUSR" '[a-zA-Z_][a-zA-Z0-9_]*$' \
+		|| die "invalid user name: $JUSR"
+	expr match "$JUID" '[1-9][0-9]*$' \
+		|| die "invalid uid: $JUID"
+
+	# Clean all files/directories in jail:
+	find . -mount -maxdepth 1 ! -name . -execdir rm -rf '{}' '+'
+
+	# Download latest-stage3-xxx.txt file, e.g.
+	# latest-stage3-amd64-hardened-nomultilib-selinux-openrc.txt:
+	#   # Latest as of Sun, 20 Mar 2022 18:00:03 +0000
+	#   # ts=1647799203
+	#   20220315T091810Z/stage3-amd64-hardened-nomultilib-selinux-openrc-20220315T091810Z.tar.xz 221397584
+	wget -O "$TDIR/STAGE3.txt" \
+		"$DSRV"/releases/"$ARCH"/autobuilds/latest-stage3-"$ARCH"-"$PROF".txt \
+		|| die
+
+	# Find filename/URL of latest stage3-xxx.tar.xz:
+	TXZ=""
+	while read A B; do
+		case "z$A" in
+		z"#"*)	continue;;
+		z*stage3*"$ARCH"-"$PROF"*.tar.xz)
+			TXZ="$A"
+			break;;
+		esac
+	done <"$TDIR/STAGE3.txt"
+	[ "z$TXZ" != "z" ] || die "cannot find latest stage3 tar.xz"
+
+	# Basename of tar.xz:
+	TXZN="${TXZ##*/}"
+
+	# Download tar.xz and signature files:
+	wget -cO "/var/tmp/$TXZN" \
+		"$DSRV"/releases/"$ARCH"/autobuilds/"$TXZ" \
+		|| die
+	wget -O "/var/tmp/$TXZN".asc \
+		"$DSRV"/releases/"$ARCH"/autobuilds/"$TXZ".asc \
+		|| die
+
+	# Verify signature:
+	gpg --verify "/var/tmp/$TXZN".asc || die
+
+	# Extract tar.xz in jail dir:
+	printf 'Extracting %s\n' "$TXZN"
+	tar xpJf "/var/tmp/$TXZN" --acls --xattrs --xattrs-include='*.*' \
+		--numeric-owner --selinux --atime-preserve=system \
+		|| die
+
+	# Remove dev subdirectory and re-create it from scratch:
+	rm -rf ./dev || die
+	printf 'Re-creating minimal %s\n' "$JAIL/dev"
+	mkdev ./
+
+	# Do minimal necessary configuration of portage in jail:
+	case "z$7" in
+	zfirefox*)
+		cat >"$JAIL/etc/portage/package.use/local.use" <<EOF
 media-libs/libglvnd	X
+media-libs/libvpx	postproc
+x11-libs/cairo		X
 EOF
-# This is for telegram:
-cat >"$JAIL/etc/portage/package.use/local.use" <<EOF
+		;;
+	ztelegram*)
+		cat >"$JAIL/etc/portage/package.use/local.use" <<EOF
 dev-qt/qtgui		dbus jpeg
+media-libs/libglvnd	X
 media-video/ffmpeg	opus
 sys-libs/zlib		minizip
 x11-libs/libxkbcommon	X
-media-libs/libglvnd	X
 EOF
-if [ -d "/var/db/repos/gentoo" ]; then
-	cat >"$JAIL/etc/portage/repos.conf" <<EOF
+		nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
+			/usr/bin/emerge -av net-im/telegram-desktop
+		;;
+	z*)	# both:
+		cat >"$JAIL/etc/portage/package.use/local.use" <<EOF
+dev-qt/qtgui		dbus jpeg
+media-libs/libglvnd	X
+media-libs/libvpx	postproc
+media-video/ffmpeg	opus
+sys-libs/zlib		minizip
+x11-libs/cairo		X
+x11-libs/libxkbcommon	X
+EOF
+		;;
+	esac
+	if [ -d "/var/db/repos/gentoo" ]; then
+		cat >"$JAIL/etc/portage/repos.conf" <<EOF
 [DEFAULT]
 main-repo = gentoo
 
@@ -278,23 +361,23 @@ sync-openpgp-key-refresh-retry-delay-max = 60
 sync-openpgp-key-refresh-retry-delay-mult = 4
 sync-webrsync-verify-signature = yes
 EOF
-else
-	die "no /var/db/repos/gentoo found, manual setup needed"
-fi
+	else
+		die "no /var/db/repos/gentoo found, manual setup needed"
+	fi
 
-# Generate C.UTF-8, en_US and en_US.UTF8 locales:
-cat >>"$JAIL/etc/locale.gen" <<EOF
+	# Generate C.UTF-8, en_US and en_US.UTF8 locales:
+	cat >>"$JAIL/etc/locale.gen" <<EOF
 en_US		ISO-8859-1
 en_US.UTF-8	UTF-8
 EOF
-locale-gen -d "$JAIL" -c "$JAIL/etc/locale.gen"
+	locale-gen -d "$JAIL" -c "$JAIL/etc/locale.gen"
 
-# Set UTC as localtime (to avoid leaking local timezone to firefox):
-rm -f "$JAIL/etc/localtime"
-ln -s ../usr/share/zoneinfo/UTC "$JAIL/etc/localtime"
+	# Set UTC as localtime (to avoid leaking local timezone to firefox):
+	rm -f "$JAIL/etc/localtime"
+	ln -s ../usr/share/zoneinfo/UTC "$JAIL/etc/localtime"
 
-# Generate .inputrc for jail users (optional):
-cat >"$JAIL/root/.inputrc" <<'EOF'
+	# Generate .inputrc for jail users (optional):
+	cat >"$JAIL/root/.inputrc" <<'EOF'
 $include /etc/inputrc
 # alternate mappings for "up" and "down" to search the history
 "\e[A": history-search-backward
@@ -302,67 +385,103 @@ $include /etc/inputrc
 "\e[B": history-search-forward
 "\eOB": history-search-forward
 EOF
-cp -pr "$JAIL/root/.inputrc" "$JAIL/etc/skel/.inputrc"
+	cp -pr "$JAIL/root/.inputrc" "$JAIL/etc/skel/.inputrc"
 
-# Create inmate user/group in jail:
-groupadd -R "$JAIL"          -g 60000 foobar
-useradd  -R "$JAIL" -u 60000 -g 60000 foobar
+	# Create inmate user/group in jail:
+	groupadd -R "$JAIL"            -g "$JUID" "$JUSR"
+	useradd  -R "$JAIL" -u "$JUID" -g "$JUID" "$JUSR"
 
-# Create /etc/resolv.conf in jail:
-printf 'nameserver %s\n' "$JNET.1" >"$JAIL/etc/resolv.conf"
+	# Create /etc/resolv.conf in jail:
+	printf 'nameserver %s\n' "$JNET.1" >"$JAIL/etc/resolv.conf"
 
-# Mount /var/tmp/portage's disk-space:
-mkdir /var/tmp/portage-"$JNAM"
-chown portage:portage /var/tmp/portage-"$JNAM"
-chmod 0755 /var/tmp/portage-"$JNAM"
-mkdir "$JAIL/var/tmp/portage"
-chown portage:portage "$JAIL/var/tmp/portage"
-chmod 0755 "$JAIL/var/tmp/portage"
-mount --bind /var/tmp/portage-"$JNAM" "$JAIL/var/tmp/portage"
-mount --make-slave "$JAIL/var/tmp/portage"
+	# Mount /var/tmp/portage, /var/db/repos and /var/cache/distfiles:
+	mount_jail_var "$JAIL" "$JNAM"
 
-# Mount /var/db/repos:
-mount --bind /var/db/repos "$JAIL/var/db/repos"
-mount --make-slave "$JAIL/var/db/repos"
+	# Set jail's locale:
+	nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
+		/usr/bin/eselect locale set C.UTF8
 
-# Mount /var/cache/distfiles:
-mount --bind /var/cache/distfiles "$JAIL/var/cache/distfiles"
-mount --make-slave "$JAIL/var/cache/distfiles"
+	# Emerge firefox/telegram in jail:
+	case "z$7" in
+	zfirefox*)
+		nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
+			/usr/bin/emerge -av www-client/firefox
+		;;
+	ztelegram*)
+		nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
+			/usr/bin/emerge -av net-im/telegram-desktop
+		;;
+	esac
 
-# Mount pseudo-filesystems (for emerge):
-#mount --bind /dev "$JAIL/dev"
-#mount --make-slave "$JAIL/dev"
-#mount --bind /run "$JAIL/run"
-#mount --make-slave "$JAIL/run"
-#mount --rbind /sys "$JAIL/sys"
-#mount --make-rslave "$JAIL/sys"
+	# Umount /var/tmp/portage, /var/db/repos and /var/cache/distfiles:
+	umount_jail_var "$JAIL" "$JNAM"
+}
 
-# Set jail's locale:
-nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
-	/usr/bin/eselect locale set C.UTF8
+# USAGE: update_jail path/to/jail
+update_jail() {
+	chdir_jail "$1"
+	[ "z$JAIL" != "z" ] || die
+	[ "z$JNAM" != "z" ] || die
+	[ "z$JNET" != "z" ] || die
 
-# Emerge firefox/telegram in jail:
-nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
-	/usr/bin/emerge -av net-im/telegram-desktop
-#	/usr/bin/emerge -av www-client/firefox
+	# Update /etc/resolv.conf in jail:
+	printf 'nameserver %s\n' "$JNET.1" >"$JAIL/etc/resolv.conf"
 
-umount --recursive "$JAIL/var/db/repos"
-umount --recursive "$JAIL/var/cache/distfiles"
-umount --recursive "$JAIL/var/tmp/portage"
+	# Mount /var/tmp/portage, /var/db/repos and /var/cache/distfiles:
+	mount_jail_var "$JAIL" "$JNAM"
 
-# Transfer X11 MIT-MAGIC-COOKIE to jail inmate:
-#XDMAUTHD="/var/lib/xdm/authdir/authfiles/"
-#XDMAUTHF="`ls -t "$XDMAUTHD" | head -n1`"
-#MITMACOO="`xauth -f "$XDMAUTHD/$XDMAUTHF" list | head -n1 | awk '{print $3}'`"
-#true >"$JAIL/home/foobar/.Xauthority"
-#xauth -f "$JAIL/home/foobar/.Xauthority" add "$JNET.1:0" . "$MITMACOO"
-#chown 60000:60000 "$JAIL/home/foobar/.Xauthority"
+	# Do updates (emerge --update --deep --newuse @world):
+	nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
+		/usr/bin/emerge -vuDN @world
 
-cat <<EOF
-To run firefox/telegram, do:
-nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
-	-P="DISPLAY=$JNET.1:0" \
-	/bin/su -w DISPLAY -c firefox/or/telegram-desktop -Pl foobar
-EOF
+	# Umount /var/tmp/portage, /var/db/repos and /var/cache/distfiles:
+	umount_jail_var "$JAIL" "$JNAM"
+}
+
+# USAGE: enter_jail path/to/jail JUSR JUID
+enter_jail() {
+	chdir_jail "$1"
+	[ "z$JAIL" != "z" ] || die
+	[ "z$JNAM" != "z" ] || die
+	[ "z$JNET" != "z" ] || die
+	JUSR="$2"
+	JUID="$3"
+	expr match "$JUSR" '[a-zA-Z_][a-zA-Z0-9_]*$' \
+		|| die "invalid user name: $JUSR"
+	expr match "$JUID" '[1-9][0-9]*$' \
+		|| die "invalid uid: $JUID"
+
+	# Update /etc/resolv.conf in jail:
+	printf 'nameserver %s\n' "$JNET.1" >"$JAIL/etc/resolv.conf"
+
+	# Transfer X11 MIT-MAGIC-COOKIE to JUSR:
+	XDMAUTHD="/var/lib/xdm/authdir/authfiles/"
+	XDMAUTHF="`ls -t "$XDMAUTHD" | head -n1`"
+	MITMACOO="`xauth -f "$XDMAUTHD/$XDMAUTHF" list \
+		| head -n1 | awk '{print $3}'`"
+	true >"$JAIL/home/$JUSR/.Xauthority"
+	xauth -f "$JAIL/home/$JUSR/.Xauthority" add "$JNET.1:0" . "$MITMACOO"
+	chown "$JUID:$JUID" "$JAIL/home/$JUSR/.Xauthority"
+
+	# Enter as JUSR:
+	nsrun -impuCTn=/run/netns/ns0 -r="$JAIL" -P="LANG=C.UTF-8" \
+		-P="DISPLAY=$JNET.1:0" \
+		/bin/su -w DISPLAY -Pl "$JUSR"
+}
+
+case "z$2" in
+z|zcreate|zfirefox|ztelegram*)
+	mkjail "$1" "$ARCH" "$PROF" "$DSRV" "$JUSR" "$JUID" "$2"
+	;;
+zupdate)
+	update_jail "$1"
+	;;
+zenter)
+	enter_jail "$1" "$JUSR" "$JUID"
+	;;
+z*)
+	die "USAGE: $0 /path/to/jail {create|firefox|telegram|update|enter}"
+	;;
+esac
 
 # vi:set ft=sh tw=79 sw=8 ts=8 noet:
